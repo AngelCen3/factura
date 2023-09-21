3f6c2a1a19e2bdd501b37e4dff319a3ccfc913a2
BetoRobert <wyjawy@hotmail.com>
2023-09-20 20:41:37 -0500

    third commit

--- resources/views/livewire/show-clientes.blade.php
+++ resources/views/livewire/show-clientes.blade.php
@@ -1,3 +1,18 @@
-<div>
-    <h1>hola</h1>
+<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex gap-10 py-12">
+    <div class="w-64">
+        <a href="">Preguntar</a>
+        <ul>
+            <li>
+                <a href="">
+                    <span></span>
+                    Ejemplo
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="w-full">
+
+    </div>
+
+    <h1 class="text-white/90">hola</h1>
 </div>
